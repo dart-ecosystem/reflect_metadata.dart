3238f3e4bb7230495cefbc3d7b418ddefaf20259
copydog <775150@qq.com>
2019-11-25 13:44:24 +0800

    version: optimize code

--- lib/src/reflect_metadata_lookup.dart
+++ lib/src/reflect_metadata_lookup.dart
@@ -1,47 +1,102 @@
+//class ReflectMetadataLookup {
+//  Map<Object, Map<String, Map<String, Object>>> _dict = {};
+//
+//  void set(Object k1, String k2, String k3, Object value) {
+//    _dict[k1] ??= {};
+//    _dict[k1][k2] ??= {};
+//
+//    _dict[k1][k2][k3] = value;
+//  }
+//
+//  bool has(Object k1, String k2, String k3) {
+//    if (_dict[k1] == null || _dict[k1][k2] == null || _dict[k1][k2][k3] == null) {
+//      return false;
+//    }
+//
+//    return true;
+//  }
+//
+//  Object get(Object k1, String k2, String k3) {
+//    if (_dict[k1] == null || _dict[k1][k2] == null || _dict[k1][k2][k3] == null) {
+//      return null;
+//    }
+//
+//    return _dict[k1][k2][k3];
+//  }
+//
+//  Object getKeys(Object k1, String k2) {
+//    if (_dict[k1] == null || _dict[k1][k2] == null) {
+//      return null;
+//    }
+//
+//    return _dict[k1][k2].keys.toList();
+//  }
+//
+//  bool delete(Object k1, String k2, String k3) {
+//    if (!has(k1, k2, k3)) {
+//      return false;
+//    }
+//
+//    _dict[k1][k2][k3] = null;
+//    return true;
+//  }
+//}
+
 class ReflectMetadataLookup {
-  Map<Object, Map<String, Map<String, Object>>> _dict = {};
+  Map<int, Object> _dict = {};
+
+  Map<int, Set<String>> _keysDict = {};
+
+  int _getKeyHashCodeFromTwoKey(int k1, int k2) {
+    return k1 * 31 + k2;
+  }
+
+  int _getKeyHashCodeFromThreeKey(int k1, int k2, int k3) {
+    return k1 * 31 * 31 + k2 * 31 + k3;
+  }
+
+  void _setKey(int hashCode, String key) {
+    _keysDict[hashCode] ??= {};
+    _keysDict[hashCode].add(key);
+  }
+
+  void _delKey(int hashCode) {
+    _keysDict.remove(hashCode);
+  }
 
   void set(Object k1, String k2, String k3, Object value) {
-    _dict[k1] ??= {};
-    _dict[k1][k2] ??= {};
-
-    _dict[k1][k2][k3] = value;
+    int keyHashCode = _getKeyHashCodeFromTwoKey(k1.hashCode, k2.hashCode);
+    int hashCode = _getKeyHashCodeFromTwoKey(keyHashCode, k3.hashCode);
+    _setKey(keyHashCode, k3);
+    _dict[hashCode] = value;
   }
 
   bool has(Object k1, String k2, String k3) {
-    if (_dict[k1] == null ||
-        _dict[k1][k2] == null ||
-        _dict[k1][k2][k3] == null) {
-      return false;
-    }
+    int hashCode = _getKeyHashCodeFromThreeKey(k1.hashCode, k2.hashCode, k3.hashCode);
 
-    return true;
+    return _dict.containsKey(hashCode);
   }
 
   Object get(Object k1, String k2, String k3) {
-    if (_dict[k1] == null ||
-        _dict[k1][k2] == null ||
-        _dict[k1][k2][k3] == null) {
-      return null;
-    }
+    int hashCode = _getKeyHashCodeFromThreeKey(k1.hashCode, k2.hashCode, k3.hashCode);
 
-    return _dict[k1][k2][k3];
+    return _dict[hashCode];
   }
 
   Object getKeys(Object k1, String k2) {
-    if (_dict[k1] == null || _dict[k1][k2] == null) {
-      return null;
-    }
-
-    return _dict[k1][k2].keys.toList();
+    int hashCode = _getKeyHashCodeFromTwoKey(k1.hashCode, k2.hashCode);
+    return _keysDict[hashCode]?.toList() ?? [];
   }
 
   bool delete(Object k1, String k2, String k3) {
     if (!has(k1, k2, k3)) {
       return false;
     }
+    int keyHashCode = _getKeyHashCodeFromTwoKey(k1.hashCode, k2.hashCode);
+    int hashCode = _getKeyHashCodeFromTwoKey(keyHashCode, k3.hashCode);
 
-    _dict[k1][k2][k3] = null;
+    _delKey(keyHashCode);
+    _dict.remove(hashCode);
     return true;
   }
 }
